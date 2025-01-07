145fb2bc6f25fba3105b104a6d6ab39c6bd6376e
Miguel Ángel Delgado <migue@miguelandelgado.com>
2025-01-07 18:01:05 +0100

    Init course

---
+++ FHerrera/01-Fundamentos/app.js
@@ -0,0 +1,19 @@
+console.log("Hola Mundo");
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+console.log({a});
+console.log({b});
+console.log({x});
+
+console.table([a, b, c, d, x]);
+
+console.info(a);
+
+c = "Hola de nuevo";    
